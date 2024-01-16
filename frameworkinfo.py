2becd235d69cfda1f8c48ecb3ff1c043c2f61c69
nik gaffney <nik@fo.am>
2024-01-16 13:16:23 +0100

    except

--- frameworkinfo.py
+++ frameworkinfo.py
@@ -8,20 +8,20 @@ def check_installed(program):
     return shutil.which(program)
 
 # Run the commands and store their output in variables
-if check_installed("dmidecode"):
+try check_installed("dmidecode"):
     bios_info = subprocess.check_output("sudo dmidecode | grep -A3 'Vendor:'", shell=True)
     system_info = subprocess.check_output("sudo dmidecode | grep -A7 'System Information'", shell=True)
-else:
+except subprocess.CalledProcessError:
     print("dmidecode is not installed")
 
-if check_installed("lshw"):
+try check_installed("lshw"):
     cpu_info = subprocess.check_output("sudo lshw -C cpu | grep -A3 'product:'", shell=True)
-else:
+except subprocess.CalledProcessError:
     print("lshw is not installed")
 
-if check_installed("uname"):
+try check_installed("uname"):
     kernel_version = subprocess.check_output("uname -r", shell=True)
-else:
+except subprocess.CalledProcessError::
     print("uname is not installed")
 
 
